f66e54ded509ddd48c825ae091f3fbd3e93aad59
paulmulcahy <paulwmulcahy@gmail.com>
2022-05-01 07:05:31 -0700

    Java Brains - REST Web Services 19 - Implementing POST Method

--- src/main/java/ca/pmulcahy/messenger/resources/MessageResource.java
+++ src/main/java/ca/pmulcahy/messenger/resources/MessageResource.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import ca.pmulcahy.messenger.model.Message;
 import ca.pmulcahy.messenger.service.MessageService;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -27,6 +29,13 @@ public class MessageResource {
 		return messageService.getAllMessages();
 	}
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message addMessage(Message message) {
+    	return messageService.addMessage(message);
+    }
+    
     @GET
     @Path("/{messageId}")
     @Produces(MediaType.APPLICATION_JSON)
